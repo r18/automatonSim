594393fa3f4c812010f0200bdc292463d0e6d9ac
r18 <twkgr424@yahoo.co.jp>
2014-05-29 14:58:41 +0900

    Add js files

---
+++ js/FA.js
@@ -0,0 +1,9 @@
+function FA() {
+ 
+}
+
+FA.pe = {
+  state:[],
+  rule:{},
+  
+}
---
+++ js/main.js
@@ -0,0 +1,5 @@
+function main() {
+  frame = SVG('mainFrame').size(640,480);
+  var rect = frame.rect(200,200).attr({fill:"#f05"});
+  c = frame.circle(100);
+}
